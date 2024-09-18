a151c5766dae86607346f4a5a3a6c2e489d796eb
onobrain <dreemerups@gmail.com>
2024-09-19 00:20:47 +0100

    deployement bug fix3

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -10,6 +10,6 @@ export const client = createClient({
 
 // converting images to url.
 const builder = ImageUrlBuilder(client);
-export function urlFor(source: any) {
+export function urlFor(source: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
   return builder.image(source);
 }
