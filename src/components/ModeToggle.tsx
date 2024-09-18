445ae3433b37c0e724cd0c95a559ff114843dff3
onobrain <dreemerups@gmail.com>
2024-09-19 00:04:50 +0100

    deployement bug fix

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -5,7 +5,6 @@ import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import { DropdownMenuIcon } from "@radix-ui/react-icons"
 import {DropdownMenu , DropdownMenuTrigger , DropdownMenuContent , DropdownMenuItem} from "@/components/ui/dropdown-menu"
 export function ModeToggle() {
   const { setTheme } = useTheme()
--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -1,14 +1,22 @@
-// data types.
+// data types
 export interface simpleBlogCard {
-    title : string;
-    smallDescription : string;
-    currentSlug : string
-    titleImage : any
+    title: string;
+    smallDescription: string;
+    currentSlug: string;
+    titleImage: {
+        asset: {
+            url: string;  // Assuming `titleImage` is an object with an asset containing the image URL
+        };
+    };
 }
 
 export interface fullBlog {
-    currentSlug : string,
-    title : string,
-    content : any,
-    titleImage : any,
+    currentSlug: string;
+    title: string;
+    content: any[] | string;  // Adjust based on how you're handling content (e.g., an array of rich text elements or plain HTML)
+    titleImage: {
+        asset: {
+            url: string;
+        };
+    };
 }
